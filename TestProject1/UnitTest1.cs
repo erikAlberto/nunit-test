bad2927ad443d797a2c6771cb86bdc07569029cc
Erik Vargas <erikalberto8492@gmail.com>
2024-06-11 23:08:59 -0400

    completed asserts in test

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -17,26 +17,61 @@ namespace TestProject1
         [Test]
         public void Test1()
         {
-            Assert.Equals(driver.FindElement(By.CssSelector(".btn-secondary")).GetAttribute("href"), "https://bit.ly/CrediseguroWhats");
-
+            //Assert.AreEqual(driver.FindElement(By.CssSelector(".btn-secondary")).GetAttribute("href"), "https://bit.ly/CrediseguroWhats");
+            Assert.AreEqual(driver.FindElement(By.CssSelector(".btn-secondary")).GetAttribute("href"), "https://bit.ly/CrediseguroWhats");
         }
 
         [Test]
         public void Test2()
         {
             driver.FindElement(By.CssSelector("a.menu-trigger")).Click();
-            driver.FindElement(By.LinkText("/seguros-individuales")).Click();
-
-            Assert.Equals(driver.FindElement(By.CssSelector("#content a")).GetDomAttribute("href"), "https://check.com.bo/");
+            driver.FindElement(By.CssSelector("#menu > div > a:nth-child(4)")).Click();
+            Assert.AreEqual(driver.FindElement(By.CssSelector("#content a")).GetDomAttribute("href"), "https://check.com.bo/");
 
         }
 
         [Test]
         public void Test3()
         {
-            driver.FindElement(By.LinkText("Verificador/Inicio")).Click();
+            driver.FindElement(By.CssSelector("#navbarNav > ul > li:nth-child(3) > a")).Click();
             driver.FindElement(By.Id("Ext")).Click();
-            driver.FindElements(By.Id("Ext"));
+            IWebElement dropdown = driver.FindElement(By.Id("Ext"));
+            IReadOnlyCollection<IWebElement> dropdownOptions = dropdown.FindElements(By.TagName("option"));
+
+            Assert.AreEqual(dropdownOptions.Count, 12);
+
+        }
+
+        [Test]
+        public void TestSelectFromDropdown()
+        {
+            driver.FindElement(By.CssSelector("#navbarNav > ul > li:nth-child(3) > a")).Click();
+            driver.FindElement(By.Id("Ext")).Click();
+            IWebElement dropdown = driver.FindElement(By.Id("Ext"));
+            IReadOnlyCollection<IWebElement> dropdownOptions = dropdown.FindElements(By.TagName("option"));
+            foreach (IWebElement option in dropdownOptions)
+            {
+                if (option.Text.Equals("PO"))
+                {
+                    option.Click();
+                }
+            }
+            string selectedOption = "";
+            foreach (IWebElement item in dropdownOptions)
+            {
+                if (item.Selected)
+                {
+                    selectedOption = item.Text;
+                }
+            }
+            Assert.That(selectedOption, Is.EqualTo("PO"));
+
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
         }
     }
 }
--- TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
+++ TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9a020db10bd3fb494646f3bd0c25e338cfdb137")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
